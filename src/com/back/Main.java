b08350c087530f96da638181434aeb1b960e3a95
HannaYang116 <apple@appleui-MacBookPro.local>
2026-04-21 13:58:45 +0900

    List ver

--- src/com/back/Main.java
+++ src/com/back/Main.java
@@ -1,64 +1,58 @@
 package com.back; // package== 폴더
 
+import java.util.ArrayList;
 import java.util.Scanner;
 //임의적_예외처리_x.ver
+
+class Saying {
+    int no;
+    String says;
+    String author;
+}
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("==명언 앱==");
         Scanner sc = new Scanner(System.in);
         String command = "";
-        String[][] arr = new String[100][2]; //[n][0]명언 [n][1]작가
-        int cnt = 0;  // 명언 개수
+        ArrayList<Saying> lst = new ArrayList<>();
+        int cnt = 0;
         while (true) {
             System.out.print("명령) ");
             command = sc.nextLine();
-            if (command.equals("종료")) {
-                break;
-            } else if (command.equals("등록")) {
+            if (command.equals("종료")) break;
+            else if (command.equals("등록")) {
+                lst.add(new Saying());
                 System.out.print("명언 : ");
-                arr[cnt][0] = sc.nextLine();
+                lst.get(cnt).says = sc.nextLine();
                 System.out.print("작가 : ");
-                arr[cnt][1] = sc.nextLine();
-                cnt++;
+                lst.get(cnt).author = sc.nextLine();
+                lst.get(cnt).no = ++cnt;
                 System.out.println(cnt + "번 명언이 등록되었습니다.");
             } else if (command.equals("목록")) {
-                System.out.println("번호 / 작가 / 명언");
-                System.out.println("----------------------");
-                int temp = cnt;
-                while (temp >= 1) {
-                    if (arr[temp - 1][0] != null) {
-                        System.out.print(temp + " / ");
-                        System.out.print(arr[temp - 1][1] + " / ");
-                        System.out.println(arr[temp - 1][0]);
-                    }
-                    temp--;
+                System.out.println("번호 / 작가 / 명언\n" + "----------------------");
+                for (int i = cnt - 1; i >= 0; i--) {
+                    if (lst.get(i).says == null) continue;
+                    else System.out.println(lst.get(i).no + " / " + lst.get(i).author + " / " + lst.get(i).says);
                 }
-            } else if (command.equals("삭제")) {
-                System.out.print("?id=");
-                int id = Integer.parseInt(sc.nextLine());
-                if (id < 1 || id > cnt || (arr[id-1][0] == null && arr[id-1][1] == null)) {
-                    System.out.println(id + "번 명언은 존재하지 않습니다.");
-                } else if (arr[id-1][0] != null || arr[id-1][1] != null) {
-                    arr[id-1][0] = null;
-                    arr[id-1][1] = null;
-                    System.out.println(id + "번 명언이 삭제되었습니다.");
+            } else if (command.startsWith("삭제?id=")) {
+                int id = Integer.parseInt(command.substring(6)) - 1;
+                if (id<0 || id>=cnt || lst.get(id).says == null) System.out.println(id + 1 + "번 명언은 존재하지 않습니다.");
+                else {
+                    lst.get(id).says = null;
+                    lst.get(id).author = null;
                 }
-            } else if (command.equals("수정")){
-                System.out.print("?id=");
-                int id = Integer.parseInt(sc.nextLine());
-                if (id < 1 || id > cnt || (arr[id-1][0] == null && arr[id-1][1] == null)) {
-                    System.out.println(id + "번 명언은 존재하지 않습니다.");
-                } else if (arr[id-1][0] != null || arr[id-1][1] != null) {
-                    System.out.println("명언(기존) : " + arr[id-1][0]);
-                    System.out.print("명언 : ");
-                    arr[id-1][0] = sc.nextLine();
-
-                    System.out.println("작가(기존) : " + arr[id-1][1]);
-                    System.out.print("작가 : ");
-                    arr[id-1][1] = sc.nextLine();
+            } else if (command.startsWith("수정?id=")) {
+                int id = Integer.parseInt((command.substring(6))) - 1;
+                if (id<0 || id>=cnt || lst.get(id).says == null) System.out.println(id + 1 + "번 명언은 존재하지 않습니다.");
+                else {
+                    System.out.print("명언(기존): " + lst.get(id).says + "\n 명언 : ");
+                    lst.get(id).says = sc.nextLine();
+                    System.out.print("작가(기존): " + lst.get(id).author + "\n 작가 : ");
+                    lst.get(id).author = sc.nextLine();
                 }
             }
-
         }
     }
 }
